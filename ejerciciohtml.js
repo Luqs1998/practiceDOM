1bc5789e027c5e01de0cb98f3d00ee4422d6fb6a
Luqs1998 <103224661+Luqs1998@users.noreply.github.com>
2022-05-05 22:57:20 -0300

    Add files via upload

--- ejerciciohtml.js
+++ ejerciciohtml.js
@@ -1,54 +1,18 @@
-let respuesta = document.getElementById("respuesta");
+let respuesta = (document.getElementById("respuesta"));
 let boton = document.getElementById("boton");
-/*
-function validar (e){
-    if (e.key == "buenos aires"){
-        let correcto = document.getElementById("correcto");
-        correcto.className = "verdad";
-        alert("Respuesta correcta maquina");  
-    }else{
 
-        let incorrecto = document.getElementById("incorrecto");
-        incorrecto.className = "falso";
-        alert("Respuesta incorrecta papu");
-    }
-}*/
 function EventoBoton (respuesta){
-    console.log(respuesta);
-    if (respuesta == "buenos aires"){
-        let correcto = document.getElementById("correcto");
+    let correcto = document.getElementById("correcto");
+    let incorrecto = document.getElementById("incorrecto");
+    let respuesta1 = respuesta.toLowerCase();
+    correcto.className = "default";
+    incorrecto.className = "default";
+    if (respuesta1 == "buenos aires"){
         correcto.className = "verdad";
-        alert("Respuesta correcta maquina");
     }else{
-        let incorrecto = document.getElementById("incorrecto");
         incorrecto.className = "falso";
-        alert("Respuesta incorrecta papu");
     }
+
 }
-console.log("esta es la respuesta" + respuesta);
+
 boton.addEventListener("click", () => EventoBoton(respuesta.value));
-
-
-/*function enviar(){
-    
-    console.log(respuesta);
-    if (respuesta == "buenos aires"){
-        let correcto = document.getElementById("correcto");
-        correcto.className = "verdad";
-        alert("Respuesta correcta maquina");
-    }else{
-        let incorrecto = document.getElementById("incorrecto");
-        incorrecto.className = "falso";
-        alert("Respuesta incorrecta papu");
-    }
-}
-*/
-
-
-/*const button = () => {
-    console.log("Hola");
-}
-let respuesta = document.getElementById("respuesta").value;
-let boton = document.getElementById("boton");
-boton.addEventListener("click", button);
-*/
